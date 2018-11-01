0a0dc41a4bf04fa9b154221a6e545b73231f7043
Jalen Davis <41176956+jalenadavis21@users.noreply.github.com>
2018-11-01 14:15:22 -0500

    React To-do Component: completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,10 +3,7 @@ import React, { Component } from 'react';
 class ToDo extends Component {
   render() {
     return (
-       <li>
-         <input type="checkbox" checked={ this.props.isCompleted }/>
-         <span>{ this.props.description }</span>
-       </li>
+       <li>A todo will go here</li>
 );
 }
 }
